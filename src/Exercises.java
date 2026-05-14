1e59223a4535d2d67acda355fb9e1dd6ac2b1049
= <=>
2026-05-14 11:49:06 -0700

    length + prepend +removeLast +min

--- src/Exercises.java
+++ src/Exercises.java
@@ -4,7 +4,7 @@ public class Exercises {
      * 
      * Returns 0 if head is null.
      * 
-     * Example: 
+     * Example:
      * Input: 9 -> 2 -> 10 -> 1
      * Output: 4
      * 
@@ -12,7 +12,16 @@ public class Exercises {
      * @return the length of the list
      */
     public static int length(ListNode head) {
-        return -1;
+        int count = 0;
+
+        ListNode current = head;
+
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
     }
 
     /**
@@ -24,12 +33,17 @@ public class Exercises {
      * 
      * Output: 4 -> 7 -> 6 -> 0
      * 
-     * @param head the head of the linked list
+     * @param head  the head of the linked list
      * @param toAdd the value to add to the front
      * @return the new head of the linked list
      */
     public static ListNode prepend(ListNode head, int toAdd) {
-        return null;
+        // Create a new Node
+        ListNode newHead = new ListNode(toAdd);
+        // Set the next of the new Node to the head
+        newHead.next = head;
+        // Return the new node
+        return newHead;
     }
 
     /**
@@ -40,12 +54,24 @@ public class Exercises {
      * Example:
      * Input: 6 -> 4 -> 9 -> 2
      * Output: 6 -> 4 -> 9
-     *  
+     * 
      * @param head the head of the linked list
      * @return the head of the list with the last element removed
      */
     public static ListNode removeLast(ListNode head) {
-        return null;
+        if (head == null) return null;
+        if (head.next == null) return null;
+        ListNode current = head;
+
+        // Find the node before the last node
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        // The next Node is the last node, set it to null
+        current.next = null;
+
+        // Return the head
+        return head;
     }
 
     /**
@@ -57,10 +83,18 @@ public class Exercises {
      * Output: -4
      * 
      * @param head the head of the linked list
-     * @return the minimum value in the list 
+     * @return the minimum value in the list
      */
     public static int min(ListNode head) {
-        return -1;
+        int smallest = Integer.MAX_VALUE;
+        ListNode current = head;
+
+        while (current != null) {
+            smallest = (current.data < smallest) ? current.data : smallest;
+            current = current.next;
+        }
+
+        return smallest;
     }
 
     /**
@@ -78,30 +112,32 @@ public class Exercises {
      * HINT: use min as a helper method
      * 
      * @param head the head of the linked list
-     * @return the head of the list with the first instance of the minimum value removed
+     * @return the head of the list with the first instance of the minimum value
+     *         removed
      */
     public static ListNode removeMin(ListNode head) {
         return null;
     }
 
     /**
-     * Returns whether two lists are of equal length AND each value in bigList is double
+     * Returns whether two lists are of equal length AND each value in bigList is
+     * double
      * that of the corresponding value in smallList. Returns false otherwise.
      * 
      * If BOTH lists are null, returns true.
      * 
      * Example:
      * smallHead: 4 -> 3 -> 9
-     * bigHead:   8 -> 6 -> 18
+     * bigHead: 8 -> 6 -> 18
      * Output: true
      * 
      * smallHead: 4 -> 3 -> 9
-     * bigHead:   8 -> 6 -> 7
+     * bigHead: 8 -> 6 -> 7
      * Output: false
      * 
      * 
      * @param smallHead the head of the list with the half values
-     * @param bigHead the head of the list with the doubled values
+     * @param bigHead   the head of the list with the doubled values
      * @return whether the values in bigList are twice the values in smallList
      */
     public static boolean isDoubled(ListNode smallList, ListNode bigList) {
@@ -119,7 +155,7 @@ public class Exercises {
      * Output: 3 -> 4 -> 5 -> 1 -> 2
      * 
      * @param head the head of the linked list
-     * @param k the number of positions to rotate
+     * @param k    the number of positions to rotate
      * @return the head of the new list after k rotations to the left
      */
     public static ListNode rotateLeft(ListNode head, int k) {
